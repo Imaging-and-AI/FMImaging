cf2a5d4d97a79af1f0f7b641811ce67bd642927f
AzR919 <azaanrehman919@gmail.com>
2023-03-17 21:37:46 -0400

    minor bug fixes and setting up init

--- cifar10/model_cifar.py
+++ cifar10/model_cifar.py
@@ -27,17 +27,22 @@ class STCNNT_Cifar(STCNNT_Base_Runtime):
         """
         super().__init__(config=config)
 
-        self.model = nn.Sequential(CNNT_Unet(config=config, total_steps=total_steps),
-                                    nn.Linear(config.C_out*config.height[0]*config.width[0], config.C_out),
+        self.unet = CNNT_Unet(config=config, total_steps=total_steps, load=False)
+        self.head = nn.Sequential(nn.Linear(config.C_out*config.height[0]*config.width[0], config.C_out),
                                     nn.LeakyReLU(),
                                     nn.Linear(config.C_out, 64),
                                     nn.LeakyReLU(),
-                                    nn.Linear(64, 10),
-                                   )
+                                    nn.Linear(64, 10))
+        
+        device = get_device(device=config.device)
+        self.set_up_optim_and_scheduling(total_steps=total_steps)
+
+        if config.load_path is not None:
+            self.load(device=device)
         
     def forward(self, x):
         """
         @args:
             - x (5D torch.Tensor): input image
         """
-        return self.model(x)
+        return self.head(self.unet(x).flatten(start_dim=1))
