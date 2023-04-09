ed6f96b7bc49a334ed77f861bbf5209b38202d33
AzR919 <azaanrehman919@gmail.com>
2023-04-09 13:36:08 -0400

    cifar tests

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 Python script to run bash scripts in batches
 """
 
+import itertools
 import subprocess
 
 # base command to run a file
@@ -28,15 +29,28 @@ cmd.extend([
 
 # commands to iterate over
 att_types = ["temporal", "local", "global"]
-cells_in_a_block = 12
-for att_type in att_types:
+a_types = ["conv", "lin"]
+mixers = ["all", "none"]
+cells_in_a_block = 3
+for att_type, a_type, mixer in itertools.product(att_types, a_types, mixers):
+    if att_type == "temporal" and a_type == "lin": continue
 
     cmd_run = cmd.copy()
-    cmd_run.extend([
-        "--run_name", f"{att_type}_{cells_in_a_block}",
-        "--run_notes", f"{cells_in_a_block}_{att_type}_cells_in_a_block",
-        "--att_types"
-    ])
+    if att_type == "temporal":
+        cmd_run.extend([
+            "--run_name", f"{att_type}_{cells_in_a_block}_{mixer}",
+            "--run_notes", f"{cells_in_a_block}_{att_type}_cells_in_a_block_w_mixer_{mixer}",
+            "--with_mixer", f"{mixer}",
+            "--att_types"
+        ])
+    else:
+        cmd_run.extend([
+            "--run_name", f"{att_type}_{cells_in_a_block}_{a_type}_{mixer}_inst_n",
+            "--run_notes", f"{cells_in_a_block}_{att_type}_cells_in_a_block_w_mixer_{mixer}_atype_{a_type}_w_instance_n",
+            "--with_mixer", f"{mixer}",
+            "--a_type", f"{a_type}",
+            "--att_types"
+        ])
     for i in range(cells_in_a_block): cmd_run.append(att_type)
 
     print(f"Running command:\n{cmd_run}")
