6c384fe62c5908eb4e63ff984e88a8eb8f0fd456
Hui Xue <hui.xue1124@gmail.com>
2023-05-05 16:14:44 -0400

    Update trainer_cifar.py
    fix the scheduler step check

--- cifar10/trainer_cifar.py
+++ cifar10/trainer_cifar.py
@@ -175,7 +175,7 @@ def trainer(rank, model, config, train_set, val_set):
                         "val_acc":val_acc})
 
             if stype == "ReduceLROnPlateau":
-                sched.step(val_acc)
+                sched.step(1.0 - val_acc)
             else: # stype == "StepLR"
                 sched.step()
 
