dd925123e6958ed9dcdfa6cd531e1e8236dbf36b
Hui Xue <hui.xue@nih.gov>
2023-05-29 17:02:59 -0400

    update run_cloud

--- mri/run_cloud.sh
+++ mri/run_cloud.sh
@@ -4,10 +4,10 @@
 nproc_per_node=4
 data_name=mri
 port=9001
-tra_ratio=100
+tra_ratio=98
 max_load=-1
 rdzv_endpoint=172.16.0.4
-nnodes=16
+nnodes=4
 
 while getopts d:e:n:p:r:m:h OPTION; do
     case "$OPTION" in
@@ -37,4 +37,4 @@ echo "node_rank: $node_rank"
 echo "python3 $HOME/mrprogs/STCNNT.git/mri/run_mri.py --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank --rdzv_id 100 --rdzv_backend c10d --rdzv_endpoint $rdzv_endpoint:$port"
 
 WANDB_API_KEY=3420ade4920debef5eb73377a2bb8f600e26a2c8
-python3 $HOME/mrprogs/STCNNT.git/mri/run_mri.py --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank --rdzv_id 100 --rdzv_backend c10d --rdzv_endpoint $rdzv_endpoint:$port --tra_ratio $tra_ratio --max_load $max_load
+python3 $HOME/mrprogs/STCNNT.git/mri/run_mri.py --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank --rdzv_id 100 --rdzv_backend c10d --rdzv_endpoint $rdzv_endpoint:$port --tra_ratio $tra_ratio --max_load $max_load --val_ratio 2
