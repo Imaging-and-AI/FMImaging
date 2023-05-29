ee7b805f6bd8c4b40ec88acc10cc3d878115ff39
Hui Xue <hui.xue@nih.gov>
2023-05-28 21:08:56 -0400

    add new testing data

--- mri/run_mri.py
+++ mri/run_mri.py
@@ -78,8 +78,8 @@ class mri_ddp_base(run_ddp_base):
         "--train_files", "train_3D_3T_retro_cine_2018.h5", "train_3D_3T_perf_2021.h5", "/export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2019.h5", "train_3D_3T_retro_cine_2020.h5",
         "--train_data_types", "2dt", "2dt", "3d", "2d",
         
-        "--test_files", "train_3D_3T_retro_cine_2020_small_3D_test.h5", "train_3D_3T_retro_cine_2020_small_2DT_test.h5", "train_3D_3T_retro_cine_2020_small_2D_test.h5",
-        "--test_data_types", "3d", "2dt", "2d"
+        "--test_files", "train_3D_3T_retro_cine_2020_small_3D_test.h5", "train_3D_3T_retro_cine_2020_small_2DT_test.h5", "train_3D_3T_retro_cine_2020_small_2D_test.h5", "train_3D_3T_retro_cine_2020_500_test.h5"
+        "--test_data_types", "3d", "2dt", "2d", "2dt"
         ])
         
         if config.tra_ratio > 0 and config.tra_ratio<=100:
