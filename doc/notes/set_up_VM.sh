396047ea28453c6cb01a9211b3b16b9504c01e75
Hui Xue <hui.xue@nih.gov>
2023-06-10 16:50:28 -0400

    set up SAS in set_up_VM.sh

--- doc/notes/set_up_VM.sh
+++ doc/notes/set_up_VM.sh
@@ -26,33 +26,39 @@ mkdir -p /export/Lab-Xue/projects/mri/data
 mkdir -p /export/Lab-Xue/projects/imagenet/data
 mkdir -p /export/Lab-Xue/projects/fm/data
 
+SAS="sp=racwdli&st=2023-06-10T20:45:41Z&se=2024-06-11T04:45:41Z&sv=2022-11-02&sr=c&sig=60Z2H8v9237zvtckS0lCa5g%2FWkkUc%2FivqhEn8KDcSmM%3D"
+
 # cine
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2018.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2018.h5?${SAS}" /export/Lab-Xue/projects/mri/data
 
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2019.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2019.h5?${SAS}" /export/Lab-Xue/projects/data
 
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020.h5?${SAS}" /export/Lab-Xue/projects/mri/data
 
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2021.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2021.h5?${SAS}" /export/Lab-Xue/projects/data
 
 ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2019.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2019.h5
 
 ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2021.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2021.h5
 
 # perfusion
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2018.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2018.h5?${SAS}" /export/Lab-Xue/projects/mri/data
 
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2019.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2019.h5?${SAS}" /export/Lab-Xue/projects/data
 
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2020.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2020.h5?${SAS}" /export/Lab-Xue/projects/data
 
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2021.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_perf_2021.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_perf_2019.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_perf_2019.h5
+
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_perf_2020.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_perf_2020.h5
 
 # test data
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_small_2DT_test.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_small_2D_test.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_small_3D_test.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
-azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_500_test.h5?sp=racwdli&st=2023-05-23T12:04:57Z&se=2026-05-23T20:04:57Z&sv=2022-11-02&sr=c&sig=t9sm9FdUUidOFspgXOP9bpaEj57kxMoQUV7p8%2FfIUUA%3D" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_small_2DT_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_small_2D_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_small_3D_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "https://stcnnt.blob.core.windows.net/mri/data/denoising/train_3D_3T_retro_cine_2020_500_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
 
 # imagenet
 azcopy copy "https://stcnnt.blob.core.windows.net/imagenet/downloaded/ILSVRC2012_img_train.tar?sp=racwdli&st=2023-05-23T12:12:36Z&se=2026-05-23T20:12:36Z&sv=2022-11-02&sr=c&sig=BD8VIaux4YSYsmkg6JdeIf1ckVAVmcGCnqlHGp93h8Y%3D" /export/Lab-Xue/projects/imagenet/data
