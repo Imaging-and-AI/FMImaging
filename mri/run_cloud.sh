fe1d797d4ab0173cdbee6bb8f39fbbc6e266f3fb
Hui Xue <hui.xue@nih.gov>
2023-06-19 16:19:37 -0400

    update run_cloud.sh

--- mri/run_cloud.sh
+++ mri/run_cloud.sh
@@ -5,20 +5,24 @@ nproc_per_node=4
 data_name=mri
 port=9001
 tra_ratio=98
+val_ratio=2
 max_load=-1
 rdzv_endpoint=172.16.0.4
 nnodes=4
+run_list=-1
 
-while getopts d:e:n:p:t:m:h OPTION; do
+while getopts d:e:n:p:t:m:v:r:h OPTION; do
     case "$OPTION" in
         d) nnodes=${OPTARG};;
         e) rdzv_endpoint=${OPTARG};;
         n) nproc_per_node=${OPTARG};;
         p) port=${OPTARG};;
         t) tra_ratio=${OPTARG};;
+        v) val_ratio=${OPTARG};;
         m) max_load=${OPTARG};;
+        r) run_list=${OPTARG};;
         h) 
-          echo "-d nnodes -e rdzv_endpoint -n nproc_per_node -p port -t tra_ratio -m max_load"
+          echo "-d nnodes -e rdzv_endpoint -n nproc_per_node -p port -t tra_ratio -v val_ratio -m max_load -r run_list"
           exit 0
         ;;
     esac
@@ -29,19 +33,21 @@ echo "rdzv_endpoint: $rdzv_endpoint"
 echo "proc per node: $nproc_per_node"
 echo "port: $port"
 echo "training data ratio: $tra_ratio"
+echo "val data ratio: $val_ratio"
 echo "max_load: $max_load"
+echo "run_list: $run_list"
 
 node_rank=$(($(hostname | sed 's/[^0-9]*//g')-1)) 
 echo "node_rank: $node_rank"
 
-~/clear_VM.sh
+bash ./doc/notes/clean_VM.sh
 
 ulimit -n 65536
 
-echo "python3 $HOME/mrprogs/STCNNT.git/mri/run_mri.py --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank --rdzv_id 100 --rdzv_backend c10d --rdzv_endpoint $rdzv_endpoint:$port"
+echo "python3 ./mri/run_mri.py --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank --rdzv_id 100 --rdzv_backend c10d --rdzv_endpoint $rdzv_endpoint:$port --run_list $run_list"
 
 WANDB_API_KEY=3420ade4920debef5eb73377a2bb8f600e26a2c8
 WANDB_MODE=offline
-python3 $HOME/mrprogs/STCNNT.git/mri/run_mri.py --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank --rdzv_id 100 --rdzv_backend c10d --rdzv_endpoint $rdzv_endpoint:$port --tra_ratio $tra_ratio --max_load $max_load --val_ratio 2 --test_ratio 100
+python3 ./mri/run_mri.py --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank --rdzv_id 100 --rdzv_backend c10d --rdzv_endpoint $rdzv_endpoint:$port --tra_ratio $tra_ratio --max_load $max_load --val_ratio $val_ratio --test_ratio 100 --run_list $run_list
 WANDB_MODE=online
-cd $HOME/mrprogs/STCNNT.git/wandb && wandb sync --sync-all
+wandb sync --sync-all ./wandb/
