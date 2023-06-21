0db90b72ebad036135eb098d473d58cf6e9fb39c
Hui Xue <hui.xue@nih.gov>
2023-06-21 08:35:54 -0400

    record running snr

--- mri/trainer_mri.py
+++ mri/trainer_mri.py
@@ -400,6 +400,7 @@ def trainer(rank, global_rank, config, wandb_run):
 
                 if wandb_run is not None:
                     wandb_run.log({"running_train_loss": loss.item()})
+                    wandb_run.log({"running_train_snr": train_snr_meter.avg})
                     wandb_run.log({"lr": curr_lr})
 
                 end_timer(enable=c.with_timer, t=tm, msg="---> logging and measuring took ")
