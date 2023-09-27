09ddd2d2ddf3ab9dc8a582099ee4f11f75116e45
Hui Xue <hui.xue@nih.gov>
2023-09-27 11:51:03 -0400

    log block str and post block str

--- mri/trainer_mri.py
+++ mri/trainer_mri.py
@@ -584,7 +584,8 @@ def trainer(rank, global_rank, config, wandb_run):
             post_block_str = c.post_mixed_unetr.block_str
 
     logging.info(f"{rank_str}, {Fore.RED}Local Rank:{rank}, global rank: {global_rank}, {c.backbone}, {c.a_type}, {c.cell_type}, {c.optim}, {c.global_lr}, {c.scheduler_type}, {c.losses}, {c.loss_weights}, weighted loss - snr {c.weighted_loss_snr} - temporal {c.weighted_loss_temporal} - added_noise {c.weighted_loss_added_noise}, data degrading {c.with_data_degrading}, snr perturb {c.snr_perturb_prob}, {c.norm_mode}, scale_ratio_in_mixer {c.scale_ratio_in_mixer}, amp {c.use_amp}, super resolution {c.super_resolution}, stride_s {c.stride_s}, separable_conv {c.separable_conv}, upsample method {c.upsample_method}, batch_size {c.batch_size}, {model_str}{Style.RESET_ALL}")
-
+    logging.info(f"{rank_str}, {Fore.RED}Local Rank:{rank}, global rank: {global_rank}, block_str, {block_str}, post_block_str, {post_block_str}{Style.RESET_ALL}")
+                 
     # -----------------------------------------------
 
     num_workers_per_loader = c.num_workers//len(train_set)
