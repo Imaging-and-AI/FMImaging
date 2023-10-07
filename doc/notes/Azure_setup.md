803e0032314c976c95b69955238ea99667088844
Hui Xue <hui.xue@nih.gov>
2023-10-06 21:52:44 -0400

    add doc

---
+++ doc/notes/Azure_setup.md
@@ -0,0 +1,176 @@
+# Set up a new Azure VM
+
+```
+# set up home folder
+sudo apt update
+mkdir ~/software
+mkdir ~/Debug
+mkdir ~/Debug/DebugOutput
+mkdir ~/gadgetron_ismrmrd_data
+ln -s ~/gadgetron_ismrmrd_data /tmp/gadgetron_data
+chmod a+rwx /tmp/gadgetron_data
+mkdir ~/mrprogs
+mkdir ~/key
+
+sudo apt install software-properties-common -y
+sudo apt install build-essential -y
+sudo apt install python3-pip emacs tmux hdparm -y
+
+cd ~ && touch .tmux.conf
+echo "set -g mouse on" >> .tmux.conf
+
+# install vscode
+#https://code.visualstudio.com/docs/setup/linux
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code -y
+
+# install cuda and gpu driver
+cd ~/software
+
+wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda_12.1.1_530.30.02_linux.run
+
+sudo sh cuda_12.1.1_530.30.02_linux.run
+
+sudo nvidia-smi -pm 1
+
+sudo reboot now
+
+# clone repo
+cd ~/.ssh
+ssh-keygen -t rsa -b 4096 -C "hui.xue@nih.gov"
+cat ./id_rsa.pub
+
+# add key to github
+cd ~/mrprogs
+git clone git@github.com:AzR919/STCNNT.git
+
+# mount and format disks
+# https://gist.github.com/keithmorris/b2aeec1ea947d4176a14c1c6a58bfc36
+
+sudo fdisk -l
+
+# test hard drive speed
+sudo hdparm -Tt /dev/nvme3n1
+
+# format and mount drives
+
+drive=(/dev/nvme0n1
+    /dev/nvme1n1
+    /dev/nvme2n1
+    /dev/nvme3n1
+    )
+
+mpoint=(
+    /export/Lab-Xue/projects/mri
+    /export/Lab-Xue/projects/imagenet
+    /export/Lab-Xue/projects/fm
+    /export/Lab-Xue/projects/data
+    )
+
+for index in ${!drive[*]}; do 
+    echo "${drive[$index]} is in ${mpoint[$index]}"
+    sudo fdisk ${drive[$index]}
+    sudo mkfs -t ext4 ${drive[$index]}
+    sudo mkdir -p ${mpoint[$index]}
+    sudo mount -t ext4 ${drive[$index]} ${mpoint[$index]}
+    sudo chmod a+rw ${mpoint[$index]}
+done
+mkdir -p /export/Lab-Xue/projects/mri/data
+mkdir -p /export/Lab-Xue/projects/imagenet/data
+mkdir -p /export/Lab-Xue/projects/fm/data
+
+# install azcopy
+cd ~/software
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
+sudo chmod a+x /usr/bin/azcopy
+
+# install packages
+
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu118
+pip3 install wandb tqdm h5py torchinfo tifffile opencv-python scikit-image einops onnx numpy scipy moviepy imageio matplotlib torchmetrics gputil onnxruntime gif
+pip3 install torch-interpol
+
+# login prompt
+# add ~/.local/bin into the path
+
+wandb login
+
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@fsi1.eastus2.cloudapp.azure.com
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@fsi2.eastus2.cloudapp.azure.com
+
+```
+
+## Create a new VM
+
+- use the azure image SFImaging/a100-general-training in the xueh2-a100-eastus2-2 group
+- Create VM, with the node type Standard NC96ads A100 v4 (96 vcpus, 880 GiB memory)
+
+## Add new VM to code remote debug
+```
+ssh -i C:/Users/xueh2/.ssh/xueh2-a100.pem gtuser@20.114.147.179 -K
+```
+
+# After creating a VM from an image
+```
+VM_name=fsi6.eastus2.cloudapp.azure.com
+
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@$VM_name "pip3 install torchmetrics colorama moviepy imageio"
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@$VM_name "sudo nvidia-smi -pm 1"
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@$VM_name "nvidia-smi"
+
+
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@$VM_name
+git clone git@github.com:AzR919/STCNNT.git /home/gtuser/mrprogs/STCNNT.git"
+
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@$VM_name "mkdir -p /export/Lab-Xue/projects/imagenet/data"
+scp -i ~/.ssh/xueh2-a100.pem /export/Lab-Xue/data/common/ILSVRC2012_img_val.tar gtuser@$VM_name:/export/Lab-Xue/projects/imagenet/data/
+scp -i ~/.ssh/xueh2-a100.pem /export/Lab-Xue/data/common/ILSVRC2012_devkit_t12.tar.gz gtuser@$VM_name:/export/Lab-Xue/projects/imagenet/data/
+scp -i ~/.ssh/xueh2-a100.pem /export/Lab-Xue/data/common/ILSVRC2012_img_train.tar gtuser@$VM_name:/export/Lab-Xue/projects/imagenet/data/
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@$VM_name "python3 -c \"import torchvision as tv; a = tv.datasets.ImageNet(root='/export/Lab-Xue/projects/imagenet/data', split='train'); a = tv.datasets.ImageNet(root='/export/Lab-Xue/projects/imagenet/data', split='val') \" "
+
+ssh -i ~/.ssh/xueh2-a100.pem gtuser@$VM_name "mkdir -p /export/Lab-Xue/projects/mri/data"
+
+```
+
+# Reinstall nvidia driver
+```
+# remote old installation if any
+sudo apt-get --purge remove cuda*
+sudo apt-get remove --purge nvidia-*
+
+# add nvidia driver ppa
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+
+# update software cache
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt-get install ubuntu-drivers-common -y
+sudo ubuntu-drivers install 535
+```
+
+# azcopy files
+```
+azcopy copy ./MINNESOTA_UHVC_RetroCine_1p5T_2022.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/MINNESOTA_UHVC_RetroCine_1p5T_2022.h5?${SAS}" --recursive
+azcopy copy ./MINNESOTA_UHVC_RetroCine_1p5T_2023.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/MINNESOTA_UHVC_RetroCine_1p5T_2023.h5?${SAS}" --recursive
+
+azcopy copy ./BWH_RetroCine_3T_2021.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_RetroCine_3T_2021.h5?${SAS}" --recursive
+azcopy copy ./BWH_RetroCine_3T_2022.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_RetroCine_3T_2022.h5?${SAS}" --recursive
+azcopy copy ./BWH_RetroCine_3T_2023.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_RetroCine_3T_2023.h5?${SAS}" --recursive
+
+azcopy copy ./BWH_Perfusion_3T_2021.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_Perfusion_3T_2021.h5?${SAS}" --recursive
+azcopy copy ./BWH_Perfusion_3T_2022.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_Perfusion_3T_2022.h5?${SAS}" --recursive
+azcopy copy ./BWH_Perfusion_3T_2023.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_Perfusion_3T_2023.h5?${SAS}" --recursive
+
+azcopy copy ./BWH_RTCine_3T_2021.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_RTCine_3T_2021.h5?${SAS}" --recursive
+azcopy copy ./BWH_RTCine_3T_2022.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_RTCine_3T_2022.h5?${SAS}" --recursive
+azcopy copy ./BWH_RTCine_3T_2023.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/BWH_RTCine_3T_2023.h5?${SAS}" --recursive
+
+azcopy copy ./VIDA_train_clean_0430.h5 "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/VIDA_train_clean_0430.h5?${SAS}" --recursive
+
+azcopy cp "./*.h5" "https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared/?${SAS}"
+```
---
+++ doc/notes/download_data.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/bash
+
+SAS="sp=racwdli&st=2023-06-10T20:45:41Z&se=2024-06-11T04:45:41Z&sv=2022-11-02&sr=c&sig=60Z2H8v9237zvtckS0lCa5g%2FWkkUc%2FivqhEn8KDcSmM%3D"
+SAS="sp=racwdli&st=2023-06-24T03:52:16Z&se=2024-06-24T11:52:16Z&spr=https&sv=2022-11-02&sr=c&sig=VMXIrGEFZEFSU6IrmxdjQSoj3wj8QTBWEE6CFzV9dic%3D"
+
+data_src=https://stcnnt.blob.core.windows.net/mri/data/denoising/data_prepared
+
+# test data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_2DT_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_2D_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_3D_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_500_samples.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+# cine
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2018.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2019.h5?${SAS}" /export/Lab-Xue/projects/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2021.h5?${SAS}" /export/Lab-Xue/projects/data
+
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2019.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2019.h5
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2021.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2021.h5
+
+azcopy copy "${data_src}/MINNESOTA_UHVC_RetroCine_1p5T_2023.h5?${SAS}" /export/Lab-Xue/projects/imagenet
+azcopy copy "${data_src}/MINNESOTA_UHVC_RetroCine_1p5T_2022.h5?${SAS}" /export/Lab-Xue/projects/imagenet
+azcopy copy "${data_src}/BWH_RetroCine_3T_2023.h5?${SAS}" /export/Lab-Xue/projects/imagenet
+azcopy copy "${data_src}/BWH_RetroCine_3T_2022.h5?${SAS}" /export/Lab-Xue/projects/imagenet
+azcopy copy "${data_src}/BWH_RetroCine_3T_2021.h5?${SAS}" /export/Lab-Xue/projects/imagenet
+
+ln -s /export/Lab-Xue/projects/imagenet/MINNESOTA_UHVC_RetroCine_1p5T_2023.h5 /export/Lab-Xue/projects/mri/data/MINNESOTA_UHVC_RetroCine_1p5T_2023.h5
+ln -s /export/Lab-Xue/projects/imagenet/MINNESOTA_UHVC_RetroCine_1p5T_2022.h5 /export/Lab-Xue/projects/mri/data/MINNESOTA_UHVC_RetroCine_1p5T_2022.h5
+ln -s /export/Lab-Xue/projects/imagenet/BWH_RetroCine_3T_2023.h5 /export/Lab-Xue/projects/mri/data/BWH_RetroCine_3T_2023.h5
+ln -s /export/Lab-Xue/projects/imagenet/BWH_RetroCine_3T_2022.h5 /export/Lab-Xue/projects/mri/data/BWH_RetroCine_3T_2022.h5 
+ln -s /export/Lab-Xue/projects/imagenet/BWH_RetroCine_3T_2021.h5 /export/Lab-Xue/projects/mri/data/BWH_RetroCine_3T_2021.h5 
+
+azcopy copy "${data_src}/BARTS_RetroCine_3T_2023.h5?${SAS}" /export/Lab-Xue/projects/fm
+azcopy copy "${data_src}/BARTS_RetroCine_1p5T_2023.h5?${SAS}" /export/Lab-Xue/projects/fm
+
+ln -s /export/Lab-Xue/projects/fm/BARTS_RetroCine_3T_2023.h5 /export/Lab-Xue/projects/mri/data/BARTS_RetroCine_3T_2023.h5
+ln -s /export/Lab-Xue/projects/fm/BARTS_RetroCine_1p5T_2023.h5 /export/Lab-Xue/projects/mri/data/BARTS_RetroCine_1p5T_2023.h5
+
+exit 0
+
+# perfusion
+azcopy copy "${data_src}/train_3D_3T_perf_2018.h5?${SAS}" /export/Lab-Xue/projects/fm
+azcopy copy "${data_src}/train_3D_3T_perf_2019.h5?${SAS}" /export/Lab-Xue/projects/fm
+azcopy copy "${data_src}/train_3D_3T_perf_2020.h5?${SAS}" /export/Lab-Xue/projects/fm
+azcopy copy "${data_src}/train_3D_3T_perf_2021.h5?${SAS}" /export/Lab-Xue/projects/fm
+
+ln -s /export/Lab-Xue/projects/fm/train_3D_3T_perf_2018.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_perf_2018.h5
+ln -s /export/Lab-Xue/projects/fm/train_3D_3T_perf_2019.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_perf_2019.h5
+ln -s /export/Lab-Xue/projects/fm/train_3D_3T_perf_2020.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_perf_2020.h5
+ln -s /export/Lab-Xue/projects/fm/train_3D_3T_perf_2021.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_perf_2021.h5
+
+azcopy copy "${data_src}/BWH_Perfusion_3T_2023.h5?${SAS}" /export/Lab-Xue/projects/fm
+azcopy copy "${data_src}/BWH_Perfusion_3T_2022.h5?${SAS}" /export/Lab-Xue/projects/fm
+azcopy copy "${data_src}/BWH_Perfusion_3T_2021.h5?${SAS}" /export/Lab-Xue/projects/fm
+
+ln -s /export/Lab-Xue/projects/fm/BWH_Perfusion_3T_2023.h5 /export/Lab-Xue/projects/mri/data/BWH_Perfusion_3T_2023.h5
+ln -s /export/Lab-Xue/projects/fm/BWH_Perfusion_3T_2022.h5 /export/Lab-Xue/projects/mri/data/BWH_Perfusion_3T_2022.h5
+ln -s /export/Lab-Xue/projects/fm/BWH_Perfusion_3T_2021.h5 /export/Lab-Xue/projects/mri/data/BWH_Perfusion_3T_2021.h5
+
+# imagenet
+#azcopy copy "https://stcnnt.blob.core.windows.net/imagenet/downloaded/ILSVRC2012_img_train.tar?sp=racwdli&st=2023-05-23T12:12:36Z&se=2026-05-23T20:12:36Z&sv=2022-11-02&sr=c&sig=BD8VIaux4YSYsmkg6JdeIf1ckVAVmcGCnqlHGp93h8Y%3D" /export/Lab-Xue/projects/imagenet/data
+#azcopy copy "https://stcnnt.blob.core.windows.net/imagenet/downloaded/ILSVRC2012_devkit_t12.tar.gz?sp=racwdli&st=2023-05-23T12:12:36Z&se=2026-05-23T20:12:36Z&sv=2022-11-02&sr=c&sig=BD8VIaux4YSYsmkg6JdeIf1ckVAVmcGCnqlHGp93h8Y%3D" /export/Lab-Xue/projects/imagenet/data
+#azcopy copy "https://stcnnt.blob.core.windows.net/imagenet/downloaded/ILSVRC2012_img_val.tar?sp=racwdli&st=2023-05-23T12:12:36Z&se=2026-05-23T20:12:36Z&sv=2022-11-02&sr=c&sig=BD8VIaux4YSYsmkg6JdeIf1ckVAVmcGCnqlHGp93h8Y%3D" /export/Lab-Xue/projects/imagenet/data
+
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_3D_test_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_2DT_test_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_2D_test_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_500_samples_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2018_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2019_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BARTS_RetroCine_3T_2023_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BARTS_RetroCine_1p5T_2023_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/MINNESOTA_UHVC_RetroCine_1p5T_2023_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/MINNESOTA_UHVC_RetroCine_1p5T_2022_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/VIDA_train_clean_0430_with_2x_resized.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+
+# cine
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2018.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2019.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2021.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+azcopy copy "${data_src}/MINNESOTA_UHVC_RetroCine_1p5T_2023.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/MINNESOTA_UHVC_RetroCine_1p5T_2022.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BWH_RetroCine_3T_2023.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BWH_RetroCine_3T_2022.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BWH_RetroCine_3T_2021.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+azcopy copy "${data_src}/BARTS_RetroCine_3T_2023.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BARTS_RetroCine_1p5T_2023.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+azcopy copy "${data_src}/train_3D_3T_perf_2018.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_perf_2019.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_perf_2020.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_perf_2021.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+azcopy copy "${data_src}/BWH_Perfusion_3T_2023.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BWH_Perfusion_3T_2022.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/BWH_Perfusion_3T_2021.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_2DT_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_2D_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_3D_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_500_samples.h5?${SAS}" /export/Lab-Xue/projects/mri/data
+
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2020_small_3D_test_with_2x_resized.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2020_small_3D_test_with_2x_resized.h5
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2020_small_2DT_test_with_2x_resized.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2020_small_2DT_test_with_2x_resized.h5
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2020_small_2D_test_with_2x_resized.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2020_small_2D_test_with_2x_resized.h5
+ln -s /export/Lab-Xue/projects/data/train_3D_3T_retro_cine_2020_500_samples_with_2x_resized.h5 /export/Lab-Xue/projects/mri/data/train_3D_3T_retro_cine_2020_500_samples_with_2x_resized.h5
+
