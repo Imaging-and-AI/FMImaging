try to fix the memory leakage

projects/qperf/qperf_data.py
@@ -33,6 +33,7 @@ class QPerfDataSet(torch.utils.data.Dataset):
     def __init__(self, data_folder, 
                         cache_folder=None,
                         max_load=-1,
+                        max_samples=-1,
                         T=80, 
                         foot_to_end=True, 
                         min_noise_level=[0.01, 0.01], 
@@ -52,6 +53,7 @@ class QPerfDataSet(torch.utils.data.Dataset):
         """
         self.data_folder = data_folder
         self.max_load = max_load
+        self.max_samples = max_samples
         self.T = T
         self.foot_to_end = foot_to_end
         self.min_noise_level = min_noise_level
@@ -98,16 +100,17 @@ class QPerfDataSet(torch.utils.data.Dataset):
                     pbar = tqdm(total=N)
                     for ind in range(mat['out'].shape[1]):
                             params = mat['out'][0,ind]['params'][0,0].flatten()
-                            aif = mat['out'][0,ind]['aif'][0,0].flatten().astype(np.float16)
-                            myo = mat['out'][0,ind]['myo'][0,0].flatten().astype(np.float16)
+                            aif = mat['out'][0,ind]['aif'][0,0].flatten().astype(np.float32)
+                            myo = mat['out'][0,ind]['myo'][0,0].flatten().astype(np.float32)
 
-                            self.aif.append(aif)
-                            self.myo.append(myo)
-                            self.params.append(params)
+                            if aif.shape[0] >= T:
+                                self.aif.append(aif)
+                                self.myo.append(myo)
+                                self.params.append(params)
 
-                            if ind % 10000 == 0:
-                                pbar.update(10000)
-                                pbar.set_description_str(f"{a_case}, {ind} out of {N}, {aif.shape[0]}, {params[0]:.4f}")
+                                if ind % 10000 == 0:
+                                    pbar.update(10000)
+                                    pbar.set_description_str(f"{a_case}, {ind} out of {N}, {aif.shape[0]}, {params[0]:.4f}")
 
                     cases_loaded += 1
                     print(f"--> {len(self.aif)} samples loaded from {cases_loaded} cases - {a_case} ... ")
@@ -123,19 +126,19 @@ class QPerfDataSet(torch.utils.data.Dataset):
             - i (int): index of the file to load
         @rets:
             - x : [T, 2] for aif and myo
-            - y : [T, 1] for clean myo
-            - p : [1, 5] for parameters (Fp, Vp, PS, Visf, delay)
+            - y : [T] for clean myo
+            - p : [5] for parameters (Fp, Vp, PS, Visf, delay)
+            - aif_p : [4], (foot, peak, valley, N)
         """
 
-        aif = np.copy(self.aif[i])
-        y = np.copy(self.myo[i])
-        params = np.copy(self.params[i])
-        p = params[:5]
-        aif_p = params[5:]
+        T = self.T
 
-        foot = int(aif_p[0])
+        aif = self.aif[i]
+        y = self.myo[i]
+        params = self.params[i]
 
         N = aif.shape[0]
+        foot = int(params[5])
 
         if self.add_noise[0]:
             # add noise to aif
@@ -157,14 +160,13 @@ class QPerfDataSet(torch.utils.data.Dataset):
                 fs = self.filter_sigma[np.random.randint(0, len(self.filter_sigma))]
                 nn = scipy.ndimage.gaussian_filter1d(nn, fs, axis=0)
 
-            myo = np.copy(y)
-            myo += nn
+            myo = y + nn
 
         x = np.concatenate((aif[:, np.newaxis], myo[:, np.newaxis]), axis=1)
 
         N = x.shape[0]
 
-        if self.foot_to_end:
+        if self.foot_to_end and foot < N/2:
             x = x[foot:, :]
             y = y[foot:]
             N = x.shape[0]
@@ -173,27 +175,20 @@ class QPerfDataSet(torch.utils.data.Dataset):
             x = x[:self.T, :]
             y = y[:self.T]
             N = x.shape[0]
-        else:
-            x_T = np.zeros((self.T, 2))
-            x_T[:N, :] = x
-            x_T[N:, :] = x[N-1, :]
+        elif N < self.T:
+            x = np.append(x, x[N-1, :] * np.ones((self.T-N, 2)), axis=0)
+            y = np.append(y, y[N-1] * np.ones(self.T-N), axis=0)
 
-            y_T = np.zeros(self.T)
-            y_T[:N] = y
-            y_T[N:] = y[N-1]
-
-            x = x_T
-            y = y_T
-
-        aif_p[3] = N
-
-        return x.astype(np.float32), y[:, np.newaxis].astype(np.float32), p[np.newaxis, :].astype(np.float32), aif_p[np.newaxis, :].astype(np.float32)
+        return x, y, np.append(params, [N], axis=0)
 
     def __len__(self):
         """
         Length of dataset
         """
-        return len(self.aif)
+        if self.max_samples>0 and len(self.aif)>self.max_samples:
+            return self.max_samples
+        else:
+            return len(self.aif)
 
     def __getitem__(self, index):
         """
@@ -267,67 +262,68 @@ if __name__ == '__main__':
                         add_noise=[True, True],
                         cache_folder='/data/qperf/cache/tra_small')
 
-    qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/val_small', 
-                        max_load=-1,
-                        T=80, 
-                        foot_to_end=True, 
-                        min_noise_level=[0.01, 0.01], 
-                        max_noise_level=[0.4, 0.15],
-                        filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
-                        only_white_noise=False,
-                        add_noise=[True, True],
-                        cache_folder='/data/qperf/cache/val_small')
+    # qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/val_small', 
+    #                     max_load=-1,
+    #                     T=80, 
+    #                     foot_to_end=True, 
+    #                     min_noise_level=[0.01, 0.01], 
+    #                     max_noise_level=[0.4, 0.15],
+    #                     filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
+    #                     only_white_noise=False,
+    #                     add_noise=[True, True],
+    #                     cache_folder='/data/qperf/cache/val_small')
 
-    qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/test_small', 
-                        max_load=-1,
-                        T=80, 
-                        foot_to_end=True, 
-                        min_noise_level=[0.01, 0.01], 
-                        max_noise_level=[0.4, 0.15],
-                        filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
-                        only_white_noise=False,
-                        add_noise=[True, True],
-                        cache_folder='/data/qperf/cache/test_small')
+    # qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/test_small', 
+    #                     max_load=-1,
+    #                     T=80, 
+    #                     foot_to_end=True, 
+    #                     min_noise_level=[0.01, 0.01], 
+    #                     max_noise_level=[0.4, 0.15],
+    #                     filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
+    #                     only_white_noise=False,
+    #                     add_noise=[True, True],
+    #                     cache_folder='/data/qperf/cache/test_small')
 
-    qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/tra', 
-                        max_load=-1,
-                        T=80, 
-                        foot_to_end=True, 
-                        min_noise_level=[0.01, 0.01], 
-                        max_noise_level=[0.4, 0.15],
-                        filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
-                        only_white_noise=False,
-                        add_noise=[True, True],
-                        cache_folder='/data/qperf/cache/tra')
+    # qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/tra', 
+    #                     max_load=-1,
+    #                     T=80, 
+    #                     foot_to_end=True, 
+    #                     min_noise_level=[0.01, 0.01], 
+    #                     max_noise_level=[0.4, 0.15],
+    #                     filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
+    #                     only_white_noise=False,
+    #                     add_noise=[True, True],
+    #                     cache_folder='/data/qperf/cache/tra')
 
-    qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/val', 
-                        max_load=-1,
-                        T=80, 
-                        foot_to_end=True, 
-                        min_noise_level=[0.01, 0.01], 
-                        max_noise_level=[0.4, 0.15],
-                        filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
-                        only_white_noise=False,
-                        add_noise=[True, True],
-                        cache_folder='/data/qperf/cache/val')
+    # qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/val', 
+    #                     max_load=-1,
+    #                     T=80, 
+    #                     foot_to_end=True, 
+    #                     min_noise_level=[0.01, 0.01], 
+    #                     max_noise_level=[0.4, 0.15],
+    #                     filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
+    #                     only_white_noise=False,
+    #                     add_noise=[True, True],
+    #                     cache_folder='/data/qperf/cache/val')
 
-    qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/test', 
-                        max_load=-1,
-                        T=80, 
-                        foot_to_end=True, 
-                        min_noise_level=[0.01, 0.01], 
-                        max_noise_level=[0.4, 0.15],
-                        filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
-                        only_white_noise=False,
-                        add_noise=[True, True],
-                        cache_folder='/data/qperf/cache/test')
+    # qperf_dataset = QPerfDataSet(data_folder='/data/qperf/mat/test', 
+    #                     max_load=-1,
+    #                     T=80, 
+    #                     foot_to_end=True, 
+    #                     min_noise_level=[0.01, 0.01], 
+    #                     max_noise_level=[0.4, 0.15],
+    #                     filter_sigma=[0.1, 0.25, 0.5, 0.8, 1.0],
+    #                     only_white_noise=False,
+    #                     add_noise=[True, True],
+    #                     cache_folder='/data/qperf/cache/test')
 
     ind = np.arange(len(qperf_dataset))
     case_lists = np.random.permutation(ind)
 
-    for t in range(20):
-        x, y, params, aif_p = qperf_dataset[case_lists[t]]
-        print(f"x - {x.shape}, y - {y.shape}, params - {params.shape}")
-        np.save(os.path.join(saved_path, f"case_{case_lists[t]}_x.npy"), x.astype(np.float32))
-        np.save(os.path.join(saved_path, f"case_{case_lists[t]}_y.npy"), y.astype(np.float32))
-        np.save(os.path.join(saved_path, f"case_{case_lists[t]}_p.npy"), params.astype(np.float32))
+    for ind, t in enumerate(case_lists):
+        x, y, params = qperf_dataset[t]
+        if ind % 10000 == 0:
+            print(f"{ind} out of {case_lists.shape[0]}, x - {x.shape}, y - {y.shape}, params - {params.shape}")
+        # np.save(os.path.join(saved_path, f"case_{case_lists[t]}_x.npy"), x.astype(np.float32))
+        # np.save(os.path.join(saved_path, f"case_{case_lists[t]}_y.npy"), y.astype(np.float32))
+        # np.save(os.path.join(saved_path, f"case_{case_lists[t]}_p.npy"), params.astype(np.float32))

projects/qperf/qperf_loss.py
@@ -56,17 +56,17 @@ class qperf_loss:
     def __call__(self, outputs, targets):
 
         y_hat, params_estimated = outputs
-        y, params, aif_p = targets
+        y, params = targets
 
-        valid_N = aif_p[:, 0, -1]
+        valid_N = params[:, -1]
 
-        B, T, C = y.shape
+        B, T = y.shape
 
-        y_hat = y_hat.to(dtype=y.dtype)
+        y_hat = y_hat.to(dtype=y.dtype).squeeze()
 
-        mask = torch.ones((B, T, 1), dtype=y.dtype).to(device=y.device)
+        mask = torch.ones((B, T), dtype=y.dtype).to(device=y.device)
         for b in range(B):
-            mask[b, int(valid_N[b]):T, 0] = 0
+            mask[b, int(valid_N[b]):T] = 0
 
         N = torch.sum(valid_N)
 
@@ -76,8 +76,8 @@ class qperf_loss:
             if not torch.isnan(v):
                 combined_loss += v
 
-        num_params = params.shape[2]
+        num_params = params_estimated.shape[1]
         for n in range(num_params):
-            combined_loss += self.config.loss_weights_params[n] * torch.sum(torch.abs(params_estimated[:, n]-params[:, :, n]))/B
+            combined_loss += self.config.loss_weights_params[n] * torch.sum(torch.abs(params_estimated[:, n]-params[:, n]))/B
 
         return combined_loss

projects/qperf/qperf_trainer.py
@@ -9,6 +9,7 @@ from time import time
 import os
 import sys
 import logging
+import gc
 
 from colorama import Fore, Back, Style
 import nibabel as nib
@@ -200,13 +201,13 @@ class QPerfTrainManager(TrainManager):
             for i, train_set_x in enumerate(self.train_sets):
                 ind = np.random.randint(0, len(train_set_x), 16)
                 for ii in ind:
-                    x, y, p, aif_p = train_set_x[ii]
+                    x, y, p = train_set_x[ii]
 
-                    Fp = p[0, 0]
-                    Vp = p[0, 1]
-                    Visf = p[0, 2]
-                    PS = p[0, 3]
-                    Delay = int(p[0, 4])
+                    Fp = p[0]
+                    Vp = p[1]
+                    Visf = p[2]
+                    PS = p[3]
+                    Delay = int(p[4])
 
                     N = x.shape[0]
 
@@ -227,6 +228,10 @@ class QPerfTrainManager(TrainManager):
 
         # Compute total iters
         total_iters = sum([len(train_loader) for train_loader in train_loaders])if not c.debug else 3
+
+        dtype = torch.float32
+        if config.use_amp:
+            dtype=torch.bfloat16
 
         # ----------------------------------------------------------------------------
         # Training loop
@@ -258,36 +263,43 @@ class QPerfTrainManager(TrainManager):
 
                         # -------------------------------------------------------
                         tm = start_timer(enable=c.with_timer)
+
                         loader_ind = idx % len(train_loader_iters)
                         loader_outputs = next(train_loader_iters[loader_ind], None)
+
                         while loader_outputs is None:
                             del train_loader_iters[loader_ind]
                             loader_ind = idx % len(train_loader_iters)
                             loader_outputs = next(train_loader_iters[loader_ind], None)
-                        x, y, p, aif_p = loader_outputs
-                        end_timer(enable=c.with_timer, t=tm, msg="---> load batch took ")
 
-                        # -------------------------------------------------------
-                        tm = start_timer(enable=c.with_timer)
+                        x, y, p = loader_outputs
+
                         x = x.to(device=device)
                         y = y.to(device)
                         p = p.to(device)
 
-                        if not c.use_amp:
-                            x = x.to(dtype=torch.float32)
-                            y = y.to(dtype=torch.float32)
-                            p = p.to(dtype=torch.float32)
+                        end_timer(enable=c.with_timer, t=tm, msg="---> load batch took ")
+
+                        # del loader_outputs
+                        # if idx % 100 == 0:
+                        #     gc.collect()
+                        # pbar.update(1)
+
+                        # continue
+
+                        # -------------------------------------------------------
+                        tm = start_timer(enable=c.with_timer)
 
                         B, T, D = x.shape
 
-                        with torch.autocast(device_type='cuda', dtype=torch.bfloat16, enabled=c.use_amp):
-                            model_output = self.model_manager(x)
+                        with torch.autocast(device_type='cuda', dtype=dtype, enabled=c.use_amp):
+                            model_output = self.model_manager(x.to(dtype=dtype))
                             y_hat, p_estimated = model_output
 
                             if torch.isnan(torch.sum(y_hat)):
                                 continue
 
-                            loss = loss_f(model_output, (y, p, aif_p))
+                            loss = loss_f(model_output, (y, p))
 
                             loss = loss / c.iters_to_accumulate
 
@@ -323,12 +335,18 @@ class QPerfTrainManager(TrainManager):
                         tm = start_timer(enable=c.with_timer)
                         curr_lr = optim.param_groups[0]['lr']
 
-                        self.metric_manager.on_train_step_end(loss.item(), model_output, loader_outputs, rank, curr_lr, False, epoch, False)
+                        loss_value = loss.detach().item()
+
+                        self.metric_manager.on_train_step_end(loss_value, model_output, loader_outputs, rank, curr_lr, False, epoch, False)
 
                         # -------------------------------------------------------
+
+                        if idx % 100 == 0:
+                            gc.collect()
+
                         pbar.update(1)
                         log_str = self.create_log_str(config, epoch, rank, 
-                                         x.shape, 
+                                         None, 
                                          self.metric_manager,
                                          curr_lr, 
                                          "tra")
@@ -336,6 +354,9 @@ class QPerfTrainManager(TrainManager):
                         pbar.set_description_str(log_str)
 
                         end_timer(enable=c.with_timer, t=tm, msg="---> epoch step logging and measuring took ")
+
+                        del x, y, p, loss, model_output, loader_outputs, y_hat, p_estimated
+                        torch.cuda.empty_cache()
                     # ------------------------------------------------------------------------------------------------------
 
                     # Run metric logging for each epoch 
@@ -345,7 +366,7 @@ class QPerfTrainManager(TrainManager):
 
                     # Print out metrics from this epoch
                     log_str = self.create_log_str(config, epoch, rank, 
-                                                x.shape, 
+                                                None, 
                                                 self.metric_manager,
                                                 curr_lr, 
                                                 "tra")
@@ -364,7 +385,7 @@ class QPerfTrainManager(TrainManager):
 
                 if c.scheduler_type != "OneCycleLR":
                     if c.scheduler_type == "ReduceLROnPlateau":
-                        sched.step(loss.item())
+                        sched.step(loss_value)
                     elif c.scheduler_type == "StepLR":
                         sched.step()
 
@@ -476,6 +497,10 @@ class QPerfTrainManager(TrainManager):
         data_loader_iters = [iter(data_loader) for data_loader in data_loaders]
         total_iters = sum([len(data_loader) for data_loader in data_loaders]) if not c.debug else 3
 
+        dtype=torch.float32
+        if c.use_amp:
+            dtype=torch.bfloat16
+
         # ------------------------------------------------------------------------
         # Evaluation loop
         with torch.inference_mode():
@@ -491,7 +516,7 @@ class QPerfTrainManager(TrainManager):
                         del data_loader_iters[loader_ind]
                         loader_ind = idx % len(data_loader_iters)
                         loader_outputs = next(data_loader_iters[loader_ind], None)
-                    x, y, p, aif_p = loader_outputs
+                    x, y, p = loader_outputs
 
                     # ----------------------------------------------------------------------
 
@@ -501,16 +526,15 @@ class QPerfTrainManager(TrainManager):
                     y = y.to(device)
                     p = p.to(device)
 
-                    if not c.use_amp:
-                        x = x.to(dtype=torch.float32)
-                        y = y.to(dtype=torch.float32)
-                        p = p.to(dtype=torch.float32)
+                    x = x.to(dtype=dtype)
+                    y = y.to(dtype=dtype)
+                    p = p.to(dtype=dtype)
 
                     # ----------------------------------------------------------------------
 
-                    with torch.autocast(device_type='cuda', dtype=torch.bfloat16, enabled=c.use_amp):
+                    with torch.autocast(device_type='cuda', dtype=dtype, enabled=c.use_amp):
                         output = self.model_manager(x)
-                        loss = loss_f(output, (y, p, aif_p))
+                        loss = loss_f(output, (y, p))
 
                     # Update evaluation metrics
                     self.metric_manager.on_eval_step_end(loss.item(), output, loader_outputs, f"{idx}", rank, save_samples, split)
@@ -521,11 +545,11 @@ class QPerfTrainManager(TrainManager):
                         samples_logged += 1
                         title = f"{id.upper()}_{samples_logged}_{x.shape}"
 
-                        Fp = p[idx, 0, 0]
-                        Vp = p[idx, 0, 1]
-                        Visf = p[idx, 0, 2]
-                        PS = p[idx, 0, 3]
-                        Delay = int(p[idx, 0, 4])
+                        Fp = p[idx, 0]
+                        Vp = p[idx, 1]
+                        Visf = p[idx, 2]
+                        PS = p[idx, 3]
+                        Delay = int(p[idx, 4])
 
                         y_hat, p_estimated = output
                         Fp_est = p_estimated[idx, 0]
@@ -536,9 +560,13 @@ class QPerfTrainManager(TrainManager):
 
                         N = x.shape[0]
 
+                        x = x.to(dtype=torch.float32).detach().cpu().numpy()
+                        y = y.to(dtype=torch.float32).detach().cpu().numpy()
+                        y_hat = y_hat.to(dtype=torch.float32).detach().cpu().numpy()
+
                         wandb.log({title : wandb.plot.line_series(
                                     xs=list(np.arange(N)),
-                                    ys=[list(x[:,0]), list(x[:,1]), list(y.flatten()), list(y_hat.flatten())],
+                                    ys=[list(x[idx,:,0]), list(x[idx,:,1]), list(y[idx,:].flatten()), list(y_hat[idx,:].flatten())],
                                     keys=["aif", "myo", "myo_clean", "myo_model"],
                                     title=f"Fp={Fp:.2f}, Vp={Vp:.2f}, Visf={Visf:.2f}, PS={PS:.2f}, Delay={Delay} - Fp={Fp_est:.2f}, Vp={Vp_est:.2f}, Visf={Visf_est:.2f}, PS={PS_est:.2f}, Delay={Delay_est}",
                                     xname="T")})
@@ -613,7 +641,7 @@ class QPerfTrainManager(TrainManager):
         else:
             loss, mse, l1, Fp, Vp, Visf, PS, Delay = loss_meters.get_eval_loss()
 
-        str= f"{Fore.GREEN}Epoch {epoch}/{config.num_epochs}, {C}{role}, {Style.RESET_ALL}{rank}, " + data_shape_str + f"{Fore.BLUE}{Back.WHITE}{Style.BRIGHT}loss {loss:.4f},{Style.RESET_ALL} {C}mse {mse:.4f}, Fp {Fp:.4f}, Vp {Vp:.4f}, Visf {Visf:.4f}, PS {PS:.4f}, Delay {Delay:.4f}{Style.RESET_ALL}{lr_str}"
+        str= f"{Fore.GREEN}Epoch {epoch}/{config.num_epochs}, {C}{role}, {Style.RESET_ALL}{rank}, " + data_shape_str + f"{Fore.BLUE}{Back.WHITE}{Style.BRIGHT}loss {loss:.4f},{Style.RESET_ALL} {C}mse {mse:.4f}, l1 {l1:.4f}, Fp {Fp:.4f}, Vp {Vp:.4f}, Visf {Visf:.4f}, PS {PS:.4f}, Delay {Delay:.4f}{Style.RESET_ALL}{lr_str}"
 
         return str
 