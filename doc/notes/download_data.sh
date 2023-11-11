898aa45100ada64836faa2f8cf8ead6037e28e3a
Hui Xue <hui.xue@nih.gov>
2023-11-10 21:29:00 -0500

    add /test_2D_sig_1_16_1000.h5?$ and /test_2DT_sig_1_16_2000.h5?$

--- doc/notes/download_data.sh
+++ doc/notes/download_data.sh
@@ -34,6 +34,9 @@ azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_2D_test.h5?${SAS}" /e
 azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_small_3D_test.h5?${SAS}" /export/Lab-Xue/projects/mri/data
 azcopy copy "${data_src}/train_3D_3T_retro_cine_2020_500_samples.h5?${SAS}" /export/Lab-Xue/projects/mri/data
 
+azcopy copy "${data_src}/test_2D_sig_1_16_1000.h5?${SAS}" /export/Lab-Xue/projects/data
+azcopy copy "${data_src}/test_2DT_sig_1_16_2000.h5?${SAS}" /export/Lab-Xue/projects/data
+
 ln -s /export/Lab-Xue/projects/data/test_2D_sig_1_16_1000.h5 /export/Lab-Xue/projects/mri/data/test_2D_sig_1_16_1000.h5
 ln -s /export/Lab-Xue/projects/data/test_2DT_sig_1_16_2000.h5 /export/Lab-Xue/projects/mri/data/test_2DT_sig_1_16_2000.h5
 
