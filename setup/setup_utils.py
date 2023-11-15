3ab03ec3a4027d39f6c3e43ccd9788acf8a04e6e
Sarah Hooper <40650255+sarahmhooper@users.noreply.github.com>
2023-11-15 10:04:20 -0600

    Remove create_generic_class_string (already in model_utils

--- setup/setup_utils.py
+++ setup/setup_utils.py
@@ -122,33 +122,6 @@ def setup_run(config):
     if config.ddp: setup_ddp()
     logging.info(f"Training on {config.device} with ddp set to {config.ddp}")
 
-# -------------------------------------------------------------------------------------------------    
-
-def create_generic_class_str(obj : object, exclusion_list=[torch.nn.Module, OrderedDict]) -> str:
-    """
-    Create a generic name of a class
-    @args:
-        - obj (object): the class to make string of
-        - exclusion_list (object list): the objects to exclude from the class string
-    @rets:
-        - class_str (str): the generic class string
-    """
-    name = type(obj).__name__
-
-    vars_list = []
-    for key, value in vars(obj).items():
-        valid = True
-        for type_e in exclusion_list:
-            if isinstance(value, type_e) or key.startswith('_'):
-                valid = False
-                break
-        
-        if valid:
-            vars_list.append(f'{key}={value!r}')
-            
-    vars_str = ',\n'.join(vars_list)
-    return f'{name}({vars_str})'
-
 # -------------------------------------------------------------------------------------------------
 
 if __name__=="__main__":
